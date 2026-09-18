8ad81eb06882d7691b461b1c95e02b66e2b95997
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-09-18 16:08:10 +0530

    by vikram

--- ADMIN/src/admin/redux/api.js
+++ ADMIN/src/admin/redux/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const API_BASE_URL = "http://localhost:8099/api";
+// const API_BASE_URL = "http://localhost:9099/api";
 const API_BASE_URL = "/api";
 
 const api = axios.create({
